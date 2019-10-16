a486d1ede1c9b91d59d36641f463e5719d82c3de
myrroddin <myrroddin@a5421a2b-9934-4a65-9552-ac7f2e9d21ed>
2019-10-16 06:45:48 +0000

    - localization should be "lua-additive-table" - Curseforge packager seems to be working again

--- trunk/LibAboutPanel-2.0.lua
+++ trunk/LibAboutPanel-2.0.lua
@@ -75,23 +75,23 @@ local L = setmetatable({}, {
 
 local locale = GetLocale()
 if locale == "koKR" then
---@localization(locale="koKR", format="lua_table")@
+--@localization(locale="koKR", format="lua_additive_table")@
 elseif locale == "frFR" then
---@localization(locale="frFR", format="lua_table")@
+--@localization(locale="frFR", format="lua_additive_table")@
 elseif locale == "deDE" then
---@localization(locale="deDE", format="lua_table")@
+--@localization(locale="deDE", format="lua_additive_table")@
 elseif locale == "ruRU" then
---@localization(locale="ruRU", format="lua_table")@
+--@localization(locale="ruRU", format="lua_additive_table")@
 elseif locale == "zhTW" then
---@localization(locale="zhTW", format="lua_table")@
+--@localization(locale="zhTW", format="lua_additive_table")@
 elseif locale == "zhCN" then
---@localization(locale="zhCN", format="lua_table")@"
+--@localization(locale="zhCN", format="lua_additive_table")@"
 elseif locale == "itIT" then
---@localization(locale="itIT", format="lua_table")@
+--@localization(locale="itIT", format="lua_additive_table")@
 elseif locale == "ptBR" then
---@localization(locale="ptBR", format="lua_table")@
+--@localization(locale="ptBR", format="lua_additive_table")@
 elseif locale == "esES" or locale == "esMX" then
---@localization(locale="esES", format="lua_table")@
+--@localization(locale="esES", format="lua_additive_table")@
 end
 
 -- handy fuction to create Title Case -----------
